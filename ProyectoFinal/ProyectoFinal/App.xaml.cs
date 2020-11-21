0f1f077854511620932613c2ad6997c67288ad64
ClaudiaMorejon <claudia.github@gmail.com>
2020-11-21 18:50:11 -0500

    login

--- ProyectoFinal/ProyectoFinal/App.xaml.cs
+++ ProyectoFinal/ProyectoFinal/App.xaml.cs
@@ -10,7 +10,7 @@ namespace ProyectoFinal
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new NavigationPage(new Login());
         }
 
         protected override void OnStart()
---
+++ ProyectoFinal/ProyectoFinal/Login.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ProyectoFinal
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Login : ContentPage
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnIngresar_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Principal());
+        }
+
+        private  void btnRegistrar_Clicked(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
